4c06ca7f2d0855f8aefa352a74fbad5103906159
Arturo Filastò <hellais@torproject.org>
2011-08-23 01:35:13 +0200

    Make it start a daemon if invoked with no arguments

--- packages/torouter-web/src/runui.py
+++ packages/torouter-web/src/runui.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
       sys.exit(2)
     sys.exit(0)
   else:
-    print "usage: %s start|stop|restart" % sys.argv[0]
-    sys.exit(2)
-
+    sys.argv[1] = '8080'
+    print "starting daemon..."
+    service.start()
 

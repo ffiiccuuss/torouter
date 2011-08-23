6f17b6fbc8f1accdfff5e5a5503baf7eafc9dc08
Arturo Filastò <hellais@torproject.org>
2011-08-23 15:15:27 +0200

    * Fix issue with distutils * Temporarily move the style sheet to base.html * Fix issues related to absolute paths * Correct pid file location * misc cleanup

--- packages/torouter-web/src/tui/view.py
+++ packages/torouter-web/src/tui/view.py
@@ -1,3 +1,4 @@
+import os
 import web
 from tui import config
 
@@ -5,7 +6,9 @@ t_globals = dict(
   datestr=web.datestr,
     )
 
+# get the path where the script currently resides
+current_path = "/".join(os.path.abspath(__file__).split("/")[:-1])
 # create the render object
-render = web.template.render('tui/views', cache=config.cache,  globals=t_globals)
+cender = web.template.render(current_path+'/views', cache=config.cache,  globals=t_globals)
 render._keywords['globals']['render'] = render
 

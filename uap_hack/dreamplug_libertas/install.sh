b55670410502720313a2317eec81924f3fdecb82
ficus <ficus@robocracy.org>
2012-09-27 16:52:30 +0200

    fix stupid typo in uap_hack install.sh

--- uap_hack/dreamplug_libertas/install.sh
+++ uap_hack/dreamplug_libertas/install.sh
@@ -5,13 +5,14 @@ set -e
 if [ -n "$1" ]
 then
     kernelversion=$1
+    echo "Installing libertas_uap for kernel version: $kernelversion"
 else
     echo "Need to specify a kernel version!" > /dev/stderr
     exit 1
 fi
 
 mkdir -p /lib/modules/$kernelversion/kernel/drivers/net/wireless/libertas_uap
-cp uap8xxx.ko-$kernerlversion /lib/modules/$kernelversion/kernel/drivers/net/wireless/libertas_uap/uap8xxx.ko
+cp uap8xxx.ko-$kernelversion /lib/modules/$kernelversion/kernel/drivers/net/wireless/libertas_uap/uap8xxx.ko
 depmod -a
 cp -r firmware/mrvl /lib/firmware/mrvl
 cp modprobe_libertas_uap.conf /etc/modprobe.d/libertas_uap
